c823c73932b46e73cceb57b723a96dedeefa13d0
Tek saud <101441449+tek1saud@users.noreply.github.com>
2023-09-08 15:56:36 +0000

    second commit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,11 @@
+
+// const accountId =3232;
+let accountEmail="saud00@gmial.com";
+var accountpassword= "1244";
+accountEmail="happy23@gmail.com"
+accountpassword="234lls"
+accountCity="kathmandu"
+accountId=3434;
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountId,accountEmail, accountpassword,accountCity])
