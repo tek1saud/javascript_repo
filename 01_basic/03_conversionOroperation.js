da4b03aebf7610f552ea09d9bc4193856a10bee5
Tek saud <101441449+tek1saud@users.noreply.github.com>
2023-09-08 18:03:31 +0000

    some basic opeartion

--- 01_basic/03_conversionOroperation.js
+++ 01_basic/03_conversionOroperation.js
@@ -1,18 +1,33 @@
-let score ="33aa";
-console.log(typeof score);
-console.log(typeof (score));
+// let score ="33aa";
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log( typeof valueInNumber);
-console.log( valueInNumber);
+// let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+// console.log( valueInNumber);
 
-let a = undefined;
-console.log( typeof a);
-let value2InNumber = Number(a);
-console.log( typeof value2InNumber);
+// let a = undefined;
+// console.log( typeof a);
+// let value2InNumber = Number(a);
+// console.log( typeof value2InNumber);
 
 // 33=>33
 //"tek"=>NaN
 // ture=>1
 // null=>0
 // undefined=>undefined
+
+
+// ****************Now time for discussion about operation****************
+// let value =2;
+// let negvalue=-value;
+// console.log(typeof negvalue);
+// console.log(negvalue);
+// console.log(2**3);
+// console.log(2%3);
+let str1="tek";
+let str2="saud";
+console.log(str1+str2);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
