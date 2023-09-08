69d3b7df8cb1355979817af60f1a589f4efa4028
Tek saud <101441449+tek1saud@users.noreply.github.com>
2023-09-08 17:44:45 +0000

    discussion of dataconversion

---
+++ 01_basic/03_conversionOroperation.js
@@ -0,0 +1,18 @@
+let score ="33aa";
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log( typeof valueInNumber);
+console.log( valueInNumber);
+
+let a = undefined;
+console.log( typeof a);
+let value2InNumber = Number(a);
+console.log( typeof value2InNumber);
+
+// 33=>33
+//"tek"=>NaN
+// ture=>1
+// null=>0
+// undefined=>undefined
