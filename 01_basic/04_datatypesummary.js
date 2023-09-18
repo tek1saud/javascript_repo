23f8b04a858cc137e3688f395c08ddcac92092c5
Tek saud <101441449+tek1saud@users.noreply.github.com>
2023-09-18 12:28:29 +0000

    update file

---
+++ 01_basic/04_datatypesummary.js
@@ -0,0 +1,29 @@
+// // primitive 
+
+// //  7 types of primitive data types:string, boolean, Null,undefined,Number , symbol ,bigint
+// // privimitives datat types are call by value
+
+
+// // reference type:non primitives data types:arrays , object,function
+// const bigNumber= 2343243535325253n
+// console.log(typeof(bigNumber));
+
+// const heros =["tek","saud","ram","saudis"]
+// let myObj={
+//     name:"tek",
+//     age:22,
+// }
+
+
+// const myfunction=function(){
+// console.log("Hello world");
+// }
+// console.log(typeof(myfunction));
+// console.log(typeof(myObj))
+// console.log(typeof(heros))
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+// memory:stack and heap
+// stack:primitive ka lagi stack use hunx we get a copy of original value
+// heap :non primitive  we get original reference of original value
+
+let myYoutubeName="teksaud"
---
+++ 01_basic/05_string.js
@@ -0,0 +1,18 @@
+const name= "Tek";
+const repoCount=40;
+// console.log(name+ repoCount +" value")
+
+// console.log(`Hello my name is ${name} and my repocount ${repoCount}`);
+
+
+const gameName= new String('teksaud');
+
+console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+const newString=gameName.substring(0,4)
+console.log(newString)
+let anotherString =gameName.slice(-,4)
+console.log(anotherString)
