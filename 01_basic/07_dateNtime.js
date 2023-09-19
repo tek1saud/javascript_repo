c0fb9b52e018a69562a2b4cb59b3b987d42c9d35
Tek saud <101441449+tek1saud@users.noreply.github.com>
2023-09-19 01:14:30 +0000

    array start

---
+++ 01_basic/07_dateNtime.js
@@ -0,0 +1,16 @@
+// Date
+let myDate=new Date();
+console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+// let myCreateDate=new Date(2023,8,18);
+// console.log(myCreateDate.toString())
+// let mycreatedate=new Date("2023-02-13");
+// console.log(mycreatedate.toString());
+// let myTimeStamp=Date.now()/1000;
+// console.log(myTimeStamp)
+
+let newDate=new Date()
+console.log(newDate.getMonth().toString());
